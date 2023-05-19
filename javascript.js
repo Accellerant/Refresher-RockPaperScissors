31a1df42f5be58bc16d8197ca272b03acccfd8c2
Accellerant <accellerant@gmail.com>
2023-05-19 15:24:47 -0700

    CPU choice working properly.
    The following functions were updated:
        * getComputerChoice()
            The description was Updated & the function
            now gives a string result based on cpuRandom().
    
        * cpuRandom()
            New function which will return a value of 1 - 3.
            This will be utilized by getComputerChoice().
    
        * playRound()
            Calls and logs getComputerChoice() to make sure the
            function is working properly.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,29 @@
 /*
 Randomly select between
 'Rock', 'Paper', 'Scissors',
-for the computer to utilize.
+for the computer to utilize via returning.
+
+Utilizes cpuRandom to choose between 1-3.
 */
 function getComputerChoice(){
+    switch(cpuRandom()) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3:
+            return 'scissors';
+        default:
+            return 'ERROR! SWITCH NOT BETWEEN 1-3!!!!';
+    }
+}
 
+/*
+Return a number between 1 - 3.
+Note: The + 1 at the end allows it to be 1 - 3.
+*/
+function cpuRandom() {
+    return Math.floor(Math.random() * 3) + 1;
 }
 
 /*
@@ -25,7 +44,7 @@ function computerSelection(){
 Play a single round of the game.
 */
 function playRound() {
-
+    console.log(getComputerChoice());
 }   
 
 /*
@@ -43,3 +62,6 @@ function player(type, move) {
     this.move = move;
     this.score = 0;
 }
+
+
+playRound();
