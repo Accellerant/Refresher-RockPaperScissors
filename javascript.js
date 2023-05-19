a5bd82cfae90a41656633fa94e69186d3501e3d4
Accellerant <accellerant@gmail.com>
2023-05-19 15:11:49 -0700

    Skeleton Functions & player Object Constructor

---
+++ javascript.js
@@ -0,0 +1,45 @@
+/*
+Randomly select between
+'Rock', 'Paper', 'Scissors',
+for the computer to utilize.
+*/
+function getComputerChoice(){
+
+}
+
+/*
+Returns the players selection.
+*/
+function playerSelection(){
+
+}
+
+/*
+Returns the computers selection.
+*/
+function computerSelection(){
+
+}
+
+/*
+Play a single round of the game.
+*/
+function playRound() {
+
+}   
+
+/*
+Play as many rounds as desired.
+*/
+function game(){
+
+}
+
+/*
+Object Constructor to be utilized by the PC and CPU. 
+*/
+function player(type, move) {
+    this.type = type;
+    this.move = move;
+    this.score = 0;
+}
